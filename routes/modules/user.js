e69265177ba57dc7a33317333095a54551efe46b
fiveFloorMan <harvieusegithub@gmail.com>
2022-04-02 10:42:13 +0800

    feat: add login page

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,4 +1,4 @@
-// 這裡是使用者登入頁
+// 這裡是使用者登入頁&註冊頁
 // 這裡的 url:http://localhost3000/user/XXX
 
 const express = require('express')
@@ -7,6 +7,18 @@ const router = express.Router()
 // url = http://localhost3000/user/login
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+// url = http://localhost3000/user/login
+// 接住login(btn)回傳的userLoginData
+router.post('/login', (req, res) => {
+  
+})
+
+// url = http://localhost3000/user/register
+// 註冊頁
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 module.exports = router
