0588dfe85c29a9487b84847b727f2fde1e260436
fiveFloorMan <harvieusegithub@gmail.com>
2022-04-02 10:57:43 +0800

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+// 這裡是 user 資料庫綱要
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+//匯出'User', 讓其他檔案使用
+module.exports = mongoose.model('User', userSchema)
